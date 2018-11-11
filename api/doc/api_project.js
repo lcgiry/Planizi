9bb39bb387dbb380e19d1a54a2a121b85bafa6e2
John71570 <john_lyon@hotmail.fr>
2018-11-11 18:07:07 +0100

    Update API and readme

---
+++ api/doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "title": "Doc - API Planizi",
+  "name": "Planizi API",
+  "description": "Full documentation about Planizi API",
+  "version": "0.0.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-11T15:51:41.284Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,7 +4,28 @@ var sequelize = require('../config/database/config-database').sequelize;
 
 const User = sequelize.import('../models/user.js');
 
-/* GET home page. */
+/**
+ * @apiDefine ErrorGroup
+ * @apiError AuthenticationRequired You must be authenticated.
+ * @apiError BadRequest Your request is invalid.
+ * @apiError UserNotFound The user does not exist.
+ */
+
+/**
+ * @apiGroup USER
+ * @api {get} /user/:mail Get user information
+ * @apiDescription Retrieve all information about an user
+ * @apiParam {String} mail The mail of the user to retrieve (ID)
+ * @apiSuccess {String} user_mail Mail of the user (ID)
+ * @apiSuccess {String} user_name Name of the user
+ * @apiSuccess {String} user_surname Surname of the user
+ * @apiSuccess {String} user_nickname Nickname of the user
+ * @apiSuccess {String} user_gender The gender ('m' or 'f')
+ * @apiSuccess {String} user_phone Phone number of the user
+ * @apiSuccess {Date} user_birthdate Birthdate of the user
+ * @apiUse ErrorGroup
+ */
+
 router.get('/mail/:id', function(req, res, next) {
 
 	User.findOne({ where: {user_mail: req.params.id}, raw: true })
@@ -13,7 +34,9 @@ router.get('/mail/:id', function(req, res, next) {
 				res.type('json');
 				res.send(userResult);
 			}else{
-				res.status(404).end();
+				res.send({
+					"error": "UserNotFound"
+				})
 			}
 
 		})
@@ -21,8 +44,14 @@ router.get('/mail/:id', function(req, res, next) {
 			console.error("ERROR - GET /mail/id >"+err)
 		});
 
-
 });
 
+router.post('/mail/:id', function(req, res, next) {
+
+	res.send({
+		"response":"Success"
+	});
+
+});
 
 module.exports = router;
