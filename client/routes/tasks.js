8263c7bcdea1cc2ab1b53e934103ac89ab32e854
cerise6970 <alexis.saget@gmail.com>
2018-11-28 18:04:36 +0100

    api link for new task

--- client/routes/tasks.js
+++ client/routes/tasks.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const request = require("request");
+var test;
+
 
 
 router.get('/new-task', function(req, res, next) {
 
-	if(req.session.user){
+	if(!req.session.user){
 		res.redirect('/login');
 	}else{
+	
+		request('http://localhost:8080/team/teams', { json: true }, (err, resu, teams) => {
+			request('http://localhost:8080/user/users', { json: true }, (err, resu, users) => {
+				res.render('tasks/new-task', {'teams':teams, 'users':users});
 
-		res.render('tasks/new-task');
+			})
+		});	
 	}
 
 });
