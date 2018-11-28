511ef2e788adebd721f52a6fc26fe08b2caa180a
fabienlenoble <fabien.lenoble@insa-lyon.fr>
2018-11-28 17:35:49 +0100

    small changes

--- client/routes/tasks.js
+++ client/routes/tasks.js
@@ -20,7 +20,19 @@ router.get('/tasks', function(req, res, next) {
 		res.redirect('/login');
 	}else{
 
-		res.render('tasks/list-tasks');
+		res.render('tasks/list-tasks', {
+			tasks: [
+				{task_label: 'Sortir les poubelles', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les avions', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les serviettes', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les animaux', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les pantalons', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les écrans', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les mobilettes', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les esquimaux', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+				{task_label: 'Sortir les couverts', task_description: 'Récupérer les poubelles pleines et les sortir', task_location: 'BMC', task_team: '', task_supervisor: 'John Ratignier', task_supervisor_phone: '06 15 24 65 45'},
+			]
+		});
 	}
 
 });
